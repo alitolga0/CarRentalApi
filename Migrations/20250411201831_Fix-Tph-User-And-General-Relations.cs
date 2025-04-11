e3bc36e9058dea9981f842e4898f7e9652ed2683
alitolga0 <cakiralitolga@gmail.com>
2025-04-12 00:05:44 +0300

    refactor: migration added to support revised model structure

---
+++ Migrations/20250411201831_Fix-Tph-User-And-General-Relations.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRentalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTphUserAndGeneralRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_AspNetUsers_SellerId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_AspNetUsers_CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_AspNetUsers_UserId",
+                table: "Rentals");
+
+            migrationBuilder.DropTable(
+                name: "CarUser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "RentalId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AdminCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Permissions",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "LicenseLate",
+                table: "Cars",
+                newName: "LicensePlate");
+
+            migrationBuilder.RenameColumn(
+                name: "DailyRate",
+                table: "Cars",
+                newName: "DailyPrice");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Rentals",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Rentals",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SellerId",
+                table: "Cars",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "IdentityRole",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityRole", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_details",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tckn = table.Column<long>(type: "bigint", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Vkn = table.Column<long>(type: "bigint", nullable: true),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_user_details_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "IdentityRole",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1", "1", "Seller", "SELLER" },
+                    { "2", "2", "Customer", "CUSTOMER" },
+                    { "3", "3", "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_details_UserId",
+                table: "user_details",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_user_details_SellerId",
+                table: "Cars",
+                column: "SellerId",
+                principalTable: "user_details",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_user_details_CustomerId",
+                table: "Rentals",
+                column: "CustomerId",
+                principalTable: "user_details",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_user_details_SellerId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_user_details_CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropTable(
+                name: "IdentityRole");
+
+            migrationBuilder.DropTable(
+                name: "user_details");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Rentals");
+
+            migrationBuilder.RenameColumn(
+                name: "LicensePlate",
+                table: "Cars",
+                newName: "LicenseLate");
+
+            migrationBuilder.RenameColumn(
+                name: "DailyPrice",
+                table: "Cars",
+                newName: "DailyRate");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Rentals",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Rentals",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SellerId",
+                table: "Cars",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RentalId",
+                table: "Cars",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Cars",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdminCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Permissions",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserType",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "CarUser",
+                columns: table => new
+                {
+                    CarsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarUser", x => new { x.CarsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_CarUser_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarUser_Cars_CarsId",
+                        column: x => x.CarsId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarUser_UsersId",
+                table: "CarUser",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_AspNetUsers_SellerId",
+                table: "Cars",
+                column: "SellerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_AspNetUsers_CustomerId",
+                table: "Rentals",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_AspNetUsers_UserId",
+                table: "Rentals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
